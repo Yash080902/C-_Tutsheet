2529de3a8db3e9b4b5c2eea7a03f19a9adad2ed6
Yash Kumar Goel <goelyash65@gmail.com>
2024-05-13 19:27:19 +0530

    basics has been added and ready to 1 push

---
+++ basics1.cpp
@@ -0,0 +1,17 @@
+#include<iostream> // here we have include a libraray which is special for CIN & COUT functions 
+using namespace std; // this is completely for standards which we are using like << >>
+
+int main(){ // Procedural language 
+
+int r;
+float area; //  these are initialisation of variables in data types
+
+cin >>r; // this is input function built in
+cout<<"\n"; // this is output function built in
+
+area = 3.147*r*r; // using the variable her
+
+cout << "Area of Circle is:"<<area<<"\n";
+}
+
+
