1503e4a0beb37fd1202785009e32118adf52c36e
Yash Kumar Goel <goelyash65@gmail.com>
2024-05-16 19:12:49 +0530

    constant qualifier has been added

---
+++ constant_qualifier.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    const float pi = 3.147f; // this is the float number which is constant we can not change its value
+    float r,a;
+    cin >>r;
+    a= pi*r*r;
+    cout<<"Area of Circle is "<<a;
+    return 0;
+}
+ /* const is more better use case than #define */
+
+ /* CONSTANT POINTERS */
+/* CONSTANT REFRENCES*/
+
+
